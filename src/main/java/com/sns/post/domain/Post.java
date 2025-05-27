6630a713ac0c5b9833eed5bfc3fe2fd743ef1f3c
jongwon1591 <jongwon1591@gmail.com>
2025-05-28 05:31:43 +0900

    초기 환경 세팅

---
+++ src/main/java/com/sns/post/domain/Post.java
@@ -0,0 +1,20 @@
+package com.sns.post.domain;
+
+import lombok.Data;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@ToString
+@Data
+public class Post {
+
+    private int id;
+    private int userId;
+    private String subject;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+
+}
